c1838a650bd26fba63068927cf7ac4eb9f326928
Faruk Turgut <farukturgutiu@gmail.com>
2023-11-15 01:29:53 +0300

    Clean up the project

--- katib/Modules/PrayerHistory/PrayerHistoryController.swift
+++ katib/Modules/PrayerHistory/PrayerHistoryController.swift
@@ -5,25 +5,16 @@
 //  Created by Faruk Turgut on 26.08.2023.
 //
 
+import Combine
+import ComposableArchitecture
 import SnapKit
 import UIKit
 
 class PrayerHistoryController: UIViewController {
     
-    private lazy var prayerHistoryView: PrayerHistoryView = {
-        .init()
-    }()
-    
-    override func loadView() {
-        view = prayerHistoryView
-    }
-    
     override func viewDidLoad() {
         super.viewDidLoad()
         
         title = "Prayer History"
-        
-        view.backgroundColor = .systemGreen
-        
     }
 }
